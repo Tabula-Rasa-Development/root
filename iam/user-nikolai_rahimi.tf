f11720a4741fb467773e913609ca36cae43afa2b
Nikolai Rahimi <nikolai@tabularasa.dev>
2022-07-14 19:39:16 -0400

    fix administrator policy attachment

--- iam/user-nikolai_rahimi.tf
+++ iam/user-nikolai_rahimi.tf
@@ -2,8 +2,7 @@ resource "aws_iam_user" "nikolai_rahimi" {
   name = "nikolai.rahimi"
 }
 
-resource "aws_iam_policy_attachment" "nikolai-administrator" {
-  name       = "nikolai_rahimi-administrator-attachment"
-  users      = [aws_iam_user.nikolai_rahimi.name]
+resource "aws_iam_user_policy_attachment" "nikolai-administrator" {
+  user       = aws_iam_user.nikolai_rahimi.name
   policy_arn = data.aws_iam_policy.administrator.arn
 }
